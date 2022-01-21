458bb48d4c4ec0e1aa839513b894b9ea741e9960
ArminZahedani <armin1zahedani@gmail.com>
2022-01-21 21:11:51 +0100

    Sending a message from A to B. Encoding via serde, needs optional feature

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,57 @@ use rand_core::OsRng;
 use scicrypt::cryptosystems::curve_el_gamal::CurveElGamal;
 use scicrypt_traits::cryptosystems::AsymmetricCryptosystem;
 use scicrypt_traits::randomness::SecureRng;
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
+use scicrypt_traits::Enrichable;
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
+use std::time::Duration;
 
-fn alice(rx_alice: Receiver<Vec<u8>>, m1: u64, m2: u64) -> u32 {
+fn alice(tx_alice: Sender<Vec<u8>>, rx_alice: Receiver<Vec<u8>>, m1: u64, m2: u64) -> u32 {
+    let mut rng = SecureRng::new(OsRng);
     let received = rx_alice.recv().unwrap();
-    let pk_arr: [u8; 32] = received.try_into().unwrap();
-    let pk = curve25519_dalek::ristretto::CompressedRistretto::from_slice(&pk_arr)
-        .decompress()
-        .unwrap();
+    let pk = bincode::deserialize(&received).unwrap();
+
+    //TODO: Move this outside of Alice. Alice should eventually just have two ciphertexts.
+    let m1_curve = &Scalar::from(m1) * &RISTRETTO_BASEPOINT_POINT;
+
+    let m2_curve = &Scalar::from(m2) * &RISTRETTO_BASEPOINT_POINT;
+
+    let c1 = CurveElGamal::encrypt(&m1_curve, &pk, &mut rng);
+    let c2 = CurveElGamal::encrypt(&m2_curve, &pk, &mut rng);
+
+    let c1c2 = &c1 + &c2;
+    let encoded: Vec<u8> = bincode::serialize(&c1c2).unwrap();
+    tx_alice.send(encoded).unwrap();
+    thread::sleep(Duration::from_millis(10));
 
     0
 }
 
-fn bob(tx_bob: std::sync::mpsc::Sender<Vec<u8>>) -> u32 {
+fn bob(tx_bob: Sender<Vec<u8>>, rx_bob: Receiver<Vec<u8>>) -> u32 {
     let mut rng = SecureRng::new(OsRng);
     let (pk, sk) = CurveElGamal::generate_keys(&Default::default(), &mut rng);
-    tx_bob.send(pk.compress().to_bytes().to_vec()).unwrap();
-    
+
+    tx_bob.send(bincode::serialize(&pk).unwrap()).unwrap();
+    let received = rx_bob.recv().unwrap();
+    let ciphertext: scicrypt::cryptosystems::curve_el_gamal::CurveElGamalCiphertext =
+        bincode::deserialize(&received).unwrap();
+
+    let plain = CurveElGamal::decrypt(&ciphertext.enrich(&pk), &sk);
+    assert_eq!(plain, &Scalar::from(7u64) * &RISTRETTO_BASEPOINT_POINT);
     0
 }
 
 fn main() {
     //Alice is the main thread, Bob is the thread we create.
-    let (tx_alice, rx_bob): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel();
+    let (tx_alice, rx_bob): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
 
-    let (tx_bob, rx_alice): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel();
+    let (tx_bob, rx_alice): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = channel();
 
     thread::spawn(move || {
-        bob(tx_bob);
+        bob(tx_bob, rx_bob);
     });
-    alice(rx_alice, 5, 2);
+
+    alice(tx_alice, rx_alice, 5, 2);
 }
 
 #[cfg(test)]
